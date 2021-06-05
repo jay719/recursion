45a2f5771ca2010535e382dd137e8f19c5468d47
jay719 <javariab17@gmail.com>
2021-06-04 18:07:13 -0600

    added reverse string function

---
+++ Recursion.js
@@ -0,0 +1,17 @@
+// Fibonacci Sequence
+
+// function forLoopExample(n){
+//     for (let i = 0;){}
+// };
+
+function reverse(str){
+    if(str.length <= 1){
+        return str;
+    }
+    else {
+        console.log(str.charAt(str.length - 1))
+        return str.charAt(str.length - 1) + reverse(str.substring(0, str.length - 1));
+    }
+}
+
+reverse("test")
